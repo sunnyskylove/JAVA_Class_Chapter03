c7f4c73502dfa2a5ff61e977a3844893c2da666e
youngsun <sunnyskylove22@gmail.com>
2024-02-05 11:56:27 +0900

    [Question] 문제풀이

---
+++ src/main/java/question/question01.java
@@ -0,0 +1,46 @@
+package question;
+
+public class question01 {
+
+    public static void main(String[] args) {
+
+        int num1 = 20;
+        int num2 = 30;
+
+        int plus = num1 + num2;
+        int minus = num2 - num1;
+        int multi = num1 * num2;
+        int div = num1 / num2;
+        int mod = num1 % num2;
+
+        System.out.println("plus = " + plus);
+        System.out.println("plus = " + minus);
+        System.out.println("plus = " + multi);
+        System.out.println("plus = " + div);
+        System.out.println("plus = " + mod);
+        //
+
+        double weight = 12.5;
+        double height = 36.4;
+
+        double area = weight * height;
+        double round = (weight+height)*2;
+
+        System.out.println("면적 = " + area);
+        System.out.println("둘레 = " + round);
+        //
+
+
+
+
+
+
+
+
+
+
+
+
+
+    }
+}
